11d13286616d3ef9632d07839a685318d6732faa
bambi teague <teeegsa@gmail.com>
2021-03-11 19:16:59 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,25 +1,59 @@
+
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def self.save(name, type, db)
-    sql= <<-SQL
-      INSERT INTO pokemon (name, type) 
-      VALUES (?, ?);
-    SQL
-    db.execute(sql, name, type)
-  end
-
-  def self.find(id_num, db)
-    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
-  end
-
   def initialize(id:, name:, type:, db:)
-    @id, @name, @type, @db = id, name, type, db
+     @id, @name, @type, @db = id, name, type, db
   end
 
-
+  def self.save(name, type, db)
+   sql= <<-SQL
+     INSERT INTO pokemon (name, type) 
+     VALUES (?, ?);
+   SQL
+   db.execute(sql, name, type)
+ end
+    
+ def self.find(id_num, db)
+   pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+   Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+ end
+ 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# class Pokemon
+#   attr_accessor :id, :name, :type, :db
+
+#   def self.save(name, type, db)
+#     sql= <<-SQL
+#       INSERT INTO pokemon (name, type) 
+#       VALUES (?, ?);
+#     SQL
+#     db.execute(sql, name, type)
+#   end
+
+#   def self.find(id_num, db)
+#     pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+#     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+#   end
+
+#   def initialize(id:, name:, type:, db:)
+#     @id, @name, @type, @db = id, name, type, db
+#   end
+
+
+# end
 
 
 
